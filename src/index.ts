afeeb9113f4e6a065083d2d58b5cf69a2175bc28
Akram shaikh <155748632+shaikhakramshakil@users.noreply.github.com>
2026-04-23 11:34:17 +0530

    feat: integrate update-notifier to alert users of new versions

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
 import { run } from "./cli";
 import { setupGlobalErrorHandlers } from "./core/errors";
+import updateNotifier from "update-notifier";
+import fs from "fs";
+import path from "path";
 
 // Ensure uncaught exceptions and unhandled rejections produce useful output
 setupGlobalErrorHandlers();
+
+try {
+    const pkgPath = path.join(__dirname, "../package.json");
+    const pkg = JSON.parse(fs.readFileSync(pkgPath, "utf-8"));
+    updateNotifier({ pkg }).notify({ isGlobal: true });
+} catch (error) {
+    // Silently ignore if update notification fails
+}
 
 run().catch((err) => {
     console.error("Fatal error:", err);
